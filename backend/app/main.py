bdcbc0094992f1d387bd0def482a55225f59e931
Trevor Olmo <babooshkiz25@gmail.com>
2026-05-03 21:01:21 -0700

    Fixed scheduled jobs issues

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,26 +15,19 @@ async def lifespan(app: FastAPI):
     print("Connecting to Postgres")
     async with AsyncSessionLocal() as db:
         print("Connected to Postgres")
-        #await fetch_current_schedules_for_all_teams()
-        #await fetch_current_rosters_for_all_teams()
-        #await scrape_all_player_logs([2025, 2024, 2023, 2022, 2021])
-        #await scrape_all_player_logs([2025])
-        #await update_daily_features()
-        #await train_models()
-        #await add_current_teams_to_db()
-        #await add_old_teams_to_db()
-        #await fetch_all_season_schedules_for_all_teams()
-        
-        # scheduled refreshes at 3 am
+        await add_current_teams_to_db()
+        await add_old_teams_to_db()
+        await fetch_all_season_schedules_for_all_teams()
+        await scrape_all_player_logs([2025, 2024, 2023, 2022, 2021])
+
         scheduler = AsyncIOScheduler()
         scheduler.add_job(fetch_current_schedules_for_all_teams,trigger="cron",hour=3)
         scheduler.add_job(fetch_current_rosters_for_all_teams,trigger="cron",hour=3)
         scheduler.add_job(scrape_all_player_logs, args=[[2025]], trigger="cron",hour=3)
         scheduler.add_job(fetch_current_scores, trigger="interval", minutes=10)
         scheduler.add_job(fetch_current_player_props, trigger="cron", hour=3)
-        scheduler.add_job(update_daily_features, trigger="cron", hour=3, minutes=30)
+        scheduler.add_job(update_daily_features, trigger="cron", hour=3)
         scheduler.add_job(train_models, trigger="cron", hour=4)
-
         scheduler.start() 
         
 
